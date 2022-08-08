275774f5132598281ad9a2a5d3e35315aec7d3e9
19391 <19391@TEAMMED_ANTHONY>
2022-08-08 11:55:49 +0800

    添加项目文件。

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //[Controller]/[ActionName]/[Parameters]
+        // MVC 所用的默认 URL 路由逻辑使用如下格式来确定调用的代码：
+        //https:/localhost:{PORT}/HelloWorld/(这里是默认index的)
+        public IActionResult Index()
+        {
+            //通常返回 IActionResult 或从 ActionResult 派生的类，而不是 string 这样的类型。
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //ViewData 字典是动态对象，这意味着任何类型都可以使用。 在添加某些内容之前，ViewData 对象没有已定义的属性。
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();  
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        //ReleaseDate上的DateType
+        //户无需在日期字段中输入时间信息。
+        //仅显示日期，而非时间信息。
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+
+
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
