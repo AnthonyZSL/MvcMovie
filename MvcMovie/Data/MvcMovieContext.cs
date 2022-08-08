a56ff67e143dffa6e2e7300d187600203b4ca941
19391 <19391@TEAMMED_ANTHONY>
2022-08-08 11:55:49 +0800

    添加项目文件。

---
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    //这个库在program中通过依赖注入的方式交给框架。
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie>? Movie { get; set; }
+        //在缓存中返回实列
+
+    }
+}
